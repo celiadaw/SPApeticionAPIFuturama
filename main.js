adc4a05c1ca453ea4fe5cb3c22056678229ec769
celiadaw <celiasalamancag@gmail.com>
2021-03-03 14:32:28 +0100

    falta botón limpiar, bonustrack...

---
+++ main.js
@@ -0,0 +1,189 @@
+
+'use strict';
+
+let url = "http://futuramaapi.herokuapp.com/api/characters/";
+let count=0;
+let characterGuardado;
+let miEleccion;
+let papelera =[];
+let cajaBusqueda=document.getElementById("cajaBusqueda") ;
+// let todo =[];
+function crearCajaCharacter (character, cajaBusqueda){
+  
+    let cajaCharacter =document.createElement('div',)
+    let numId= ` character${count}`;
+    cajaCharacter.setAttribute('id', numId)
+    cajaCharacter.setAttribute("class", "listado");
+
+  
+
+    let cajaMostrar = document.getElementById('mostrarEleccion');
+    cajaMostrar.appendChild(cajaCharacter);
+    let quoteText = recogerQuote(character)
+    cajaCharacter.appendChild(quoteText);
+   
+    let imageCharacter = recogerImg(character)
+     cajaCharacter.appendChild(imageCharacter);
+
+
+        document
+         .getElementById(numId)
+          .addEventListener("click", ()=>{
+               miEleccion=document.getElementById(numId);
+               miEleccion.classList.remove("listado");
+              miEleccion.classList.add("solo");
+               borrarPapelera(papelera, miEleccion);
+               let cajaBusqueda=document.getElementById("cajaBusqueda") ;
+               cajaBusqueda.classList.remove("cajaBusqueda");
+               cajaBusqueda.classList.add("hidden");
+          })
+          
+
+    papelera.push(cajaCharacter)
+
+    count ++;
+
+  
+
+  
+    return cajaCharacter;
+
+
+}
+
+
+function recogerImg (character){
+    let image =document.createElement('img');
+    image.setAttribute('src', `${character.image}` );
+   
+     return image
+}
+
+function recogerQuote (character){
+    
+    let textQuote=  document.createTextNode(character.quote);
+  
+    return textQuote;
+   
+   
+
+}
+
+
+
+function recogerEleccion(character){
+    characterGuardado=character;
+let i=0;
+fetch(`${url}${character}`)
+.then(response => response.json())
+.then(data => {
+
+
+data.map(item => crearCajaCharacter (item))
+
+
+});
+ 
+};
+
+
+function borrarPapelera(papelera, item) {
+//en el for recorremos todos los elementos que hay en la papelera
+//borramos todos menos el que hemos seleccionado
+        for (let i = 0; i < papelera.length; i++) {
+            if(papelera[i]!=item )
+            papelera[i].remove();        
+        }
+
+    
+}
+function borrarEleccionPapelera(papelera, eleccion){
+    //esta función es para cuando vamos a
+    for (let i = 0; i < papelera.length; i++) {
+        if(papelera[i]=eleccion )
+        papelera[i].remove();        
+    }
+
+
+}
+
+
+// function cambiarCajaBusqueda (cajaBusqueda){
+//     if(cajaBusqueda.classList.contains("cajaBusqueda")){
+//         cajaBusqueda.classList.remove("cajaBusqueda");
+//         cajaBusqueda.classList.add("hidden");
+
+//     }else{
+//         cajaBusqueda.classList.remove("hidden");
+
+//         cajaBusqueda.classList.add("cajaBusqueda");
+       
+
+
+//     }
+
+
+
+// }
+//-----------------main---------------------------------
+//Botón aceptar
+document
+    .getElementById('aceptar')
+    .addEventListener("click", () =>{
+                     recogerEleccion(document
+                            .getElementById('optionCharacter').value);
+
+});
+
+//Botón Atrás
+document
+    .getElementById('atras')
+    .addEventListener("click", () =>{
+                     recogerEleccion(characterGuardado);
+                borrarEleccionPapelera(papelera, miEleccion);
+                if(cajaBusqueda.classList.contains("hidden")){
+                    cajaBusqueda.classList.remove("hidden");
+                   cajaBusqueda.classList.add("cajaBusqueda");
+
+                }
+});
+
+
+
+// let myPromise = new Promise (resolveFunction);
+// myPromise((resolveFunction, reject)=>{
+//     resolveFunction();
+// })
+
+
+//                 //Botón aceptar
+//                     .then (document
+//                             .getElementById('aceptar')
+//                             .addEventListener("click", () =>{
+//                                                  recogerEleccion(document
+//                                                         .getElementById('optionCharacter').value);
+
+//                         }))
+//                     .then (document
+//                                 .getElementById(numId)
+//                                     .addEventListener("click", ()=>{
+//                             miEleccion=document.getElementById(numId);
+
+//                             miEleccion.classList.remove("listado");
+//                             miEleccion.classList.add("solo");
+                  
+//                             borrarPapelera(papelera, miEleccion);
+
+//                                   }))
+//                     .then (//Botón Atrás
+//                             borrarEleccionPapelera(papelera, miEleccion),
+    
+//                             document
+//                                 .getElementById('atras')
+//                                 .addEventListener("click", () =>{
+//                                         recogerEleccion(characterGuardado);
+            
+//                          })
+//                          )                          
+
+        
--- prueba.js
+++ prueba.js
@@ -1,114 +1,124 @@
 
-'use strict';
 
-let url = "http://futuramaapi.herokuapp.com/api/characters/";
-let count=0;
-let character= "";
-let papelera =[];
-// let todo =[];
-function crearCajaCharacter (character){
+
+
+//EL JS ESTA EN MAIN.JS
+
+
+
+
+
+
+// 'use strict';
+
+// let url = "http://futuramaapi.herokuapp.com/api/characters/";
+// let count=0;
+// let character= "";
+// let papelera =[];
+// // let todo =[];
+// function crearCajaCharacter (character){
   
-    let cajaCharacter =document.createElement('div',)
-    let numId= ` character${count}`;
-    cajaCharacter.setAttribute('id', numId)
-    cajaCharacter.setAttribute("class", "listado");
+//     let cajaCharacter =document.createElement('div',)
+//     let numId= ` character${count}`;
+//     cajaCharacter.setAttribute('id', numId)
+//     cajaCharacter.setAttribute("class", "listado");
 
   
 
-    let cajaMostrar = document.getElementById('mostrarEleccion');
-    cajaMostrar.appendChild(cajaCharacter);
-    let quoteText = recogerQuote(character)
-    cajaCharacter.appendChild(quoteText);
+//     let cajaMostrar = document.getElementById('mostrarEleccion');
+//     cajaMostrar.appendChild(cajaCharacter);
+//     let quoteText = recogerQuote(character)
+//     cajaCharacter.appendChild(quoteText);
    
-    let imageCharacter = recogerImg(character)
-     cajaCharacter.appendChild(imageCharacter);
+//     let imageCharacter = recogerImg(character)
+//      cajaCharacter.appendChild(imageCharacter);
 
   
-    //revisar
-    document
-        .getElementById(numId)
-        .addEventListener("click", ()=>{
-                    let miEleccion=document.getElementById(numId);
+//     //revisar
+//     document
+//         .getElementById(numId)
+//         .addEventListener("click", ()=>{
+//                     let miEleccion=document.getElementById(numId);
 
-                    miEleccion.classList.remove("listado");
-                    miEleccion.classList.add("solo");
+//                     miEleccion.classList.remove("listado");
+//                     miEleccion.classList.add("solo");
                     
-                    console.log(miEleccion);
-                    console.log (papelera);
-           borrarPapelera(papelera, miEleccion);
+//                     console.log(miEleccion);
+//                     console.log (papelera);
+//            borrarPapelera(papelera, miEleccion);
 
-        });
+//         });
     
-    papelera.push(cajaCharacter);
-    console.log(papelera)
-    console.log(papelera[count]+ " posicion"+ count );
-    console.log(papelera[count]);
-    count ++;
+//     papelera.push(cajaCharacter);
+//     console.log(papelera)
+//     console.log(papelera[count]+ " posicion"+ count );
+//     console.log(papelera[count]);
+//     count ++;
 
   
 
   
-    return cajaCharacter;
+//     return cajaCharacter;
 
 
-}
+// }
 
 
-function recogerImg (character){
-    let image =document.createElement('img');
-    image.setAttribute('src', `${character.image}` );
+// function recogerImg (character){
+//     let image =document.createElement('img');
+//     image.setAttribute('src', `${character.image}` );
    
-     return image
-}
+//      return image
+// }
 
-function recogerQuote (character){
+// function recogerQuote (character){
     
-    let textQuote=  document.createTextNode(character.quote);
+//     let textQuote=  document.createTextNode(character.quote);
   
-    return textQuote;
+//     return textQuote;
    
    
 
-}
+// }
 
 
 
-function recogerEleccion(character){
-// let eleccion= ;
-let i=0;
-fetch(`${url}${character}`)
-.then(response => response.json())
-.then(data => {
+// function recogerEleccion(character){
+// // let eleccion= ;
+// let i=0;
+// fetch(`${url}${character}`)
+// .then(response => response.json())
+// .then(data => {
 
-//aquí data ahora mismo es un array
-//dentro del map recojemos 
+// //aquí data ahora mismo es un array
+// //dentro del map recojemos 
 
-data.map(item => crearCajaCharacter (item))
-// console.log("lo que añadimos al array "+papelera[i]+" posicion "+i);
-// i++;
+// data.map(item => crearCajaCharacter (item))
+// // console.log("lo que añadimos al array "+papelera[i]+" posicion "+i);
+// // i++;
 
-});
+// });
  
-};
+// };
 
 
-function borrarPapelera(papelera, item) {
+// function borrarPapelera(papelera, item) {
 
-        for (let i = 0; i < papelera.length; i++) {
-            if(papelera[i]!=item )
-            papelera[i].remove();        
-        }
-    //papelera.splice()?????????
+//         for (let i = 0; i < papelera.length; i++) {
+//             if(papelera[i]!=item )
+//             papelera[i].remove();        
+//         }
+//     //papelera.splice()?????????
     
-}
+// }
 
-//-----------------main---------------------------------
-document
-    .getElementById('aceptar')
-    .addEventListener("click", () =>{
-                     recogerEleccion(document
-                            .getElementById('optionCharacter').value);
+// //-----------------main---------------------------------
+// document
+//     .getElementById('aceptar')
+//     .addEventListener("click", () =>{
+//                      recogerEleccion(document
+//                             .getElementById('optionCharacter').value);
 
-});
+// });
 
 
